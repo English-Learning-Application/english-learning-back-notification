e63106c062726d654ac2fcd5a5921f5c2f5d3099
Nguyen Phuong Tai <521h0480@student.tdtu.edu.vn>
2025-01-27 23:35:12 +0700

    clean

---
+++ src/main/kotlin/com/security/app/controllers/NotificationController.kt
@@ -0,0 +1,13 @@
+package com.security.app.controllers
+
+import com.security.app.model.ListMessage
+import com.security.app.model.Message
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/notifications")
+class NotificationController(
+) {
+    
+}
